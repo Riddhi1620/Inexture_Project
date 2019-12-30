6065cc2dd4bfe05c80ccb6b432cbe037ed68ea92
Riddhi1620 <soniriddhi1998@gmail.com>
2019-12-30 17:05:18 +0530

    all changes are done

---
+++ Masonary_effect/js/custom.js
@@ -0,0 +1,14 @@
+
+$('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-item',
+    gutter: 20,
+    percentPosition: true,
+    resize:true,
+    stamp:'.stamp'
+    // horizontalOrder: true
+
+  });
+
+
