9a26e4e1d4181c66f116ede6b74c04cb1ab6686a
Riddhi1620 <soniriddhi1998@gmail.com>
2020-01-02 12:35:18 +0530

    new psd accrostia

---
+++ accrostia_webpage/js/custom.js
@@ -0,0 +1,11 @@
+            $(function () {
+                $('.filter').filterizr();
+            });
+
+            // $(document).ready(function(){
+            //     $('.slider-wraper').slick({
+            //         dots:true,
+            //         arrow:true
+            //     });
+            // });
+            
